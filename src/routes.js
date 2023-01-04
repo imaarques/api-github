1f6e8de9bb447f267c90b6bdf4c82b304936aeba
Igor Marques <soigormarques@gmail.com>
2023-01-04 15:50:45 -0300

    alterações

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route,
+} from 'react-router-dom';
 
 import MainPage from './pages/MainPage';
 import RepositoriesPage from './pages/RepositoriesPage';
